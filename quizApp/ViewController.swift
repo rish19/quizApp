7cfc03d996c36b777734c54184560f6863831b1d
Rishabh Bhandari <rishabh.bhandari53@gmail.com>
2019-06-03 11:51:52 +0530

    Initial Commit

---
+++ quizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quizApp
+//
+//  Created by Rishabh Bhandari on 03/06/19.
+//  Copyright © 2019 Rishabh Bhandari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
